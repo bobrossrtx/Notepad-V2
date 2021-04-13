f7ce48b579e90d84e665436790dde47c05513596
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2021-04-13 20:18:25 +0100

    Added Client

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ mongoose.connection.on('connected',
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client', 'build')));
 app.use('/api/user', require('./controllers/User'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(5000, 
