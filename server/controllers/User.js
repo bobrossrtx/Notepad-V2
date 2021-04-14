6c664f2b3872a3f880df171710a25389cec7f641
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2021-04-14 18:54:44 +0100

    Updated and working on User Controller

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
+const User = require('../models/User')
 const router = express.Router();
 
 router.get('/init', async (req, res) => {
@@ -23,46 +24,59 @@ router.get('/init', async (req, res) => {
         response = user;
     }
 
-    res.send({user: response});
+    res.send({
+        user: response
+    });
 });
 
 router.post('/register', async (req, res) => {
-    const user = await User.find({email: req.body.email});
-    console.log(user);
+    const user = await User.find({
+        email: req.body.email
+    });
     if (user._id) {
         return res.status(400).send({
             message: 'User with this email already exists'
         });
     }
-
     const newUser = User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         role: 'user'
     })
-
     await newUser.save();
-
     return res.sendStatus(201);
 });
 
-router.post('/auth/login', async (req, res) => {
-    const user = await User.find({email: req.body.email});
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email
+    });
     if (!user) {
         return res.status(400).send({
             message: 'User with this email does not exist'
         });
     }
 
-    const passwordIsEqual = await bcrypt.compare(req.body.password, user.password)
-    if(!passwordIsEqual) {
-        return res.status(401).send({
-            message: 'Please check if that is the correct password'
+    const passwordCorrect = user === null ? false :
+        await bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (err) {
+                console.error(err)
+                return
+            };
+            console.log(req.body.password, user.password, result);
+            return result;
         });
+
+    if (passwordCorrect !== true) {
+        return res.status(401).send({
+            message: "Invalid password, please try again"
+        })
     }
 
-    const token = jwt.sign({userId: user._id}, 'app');
+    const token = jwt.sign({
+        userId: user._id
+    }, 'app');
 
     res.send({
         user,
